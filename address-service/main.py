363a2bab1b0e6672ec65d6370a29403e98f224d6
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-13 11:22:09 -0500

    address: Add uvicorn to deploy service

--- address-service/main.py
+++ address-service/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 
@@ -7,3 +8,7 @@ app = FastAPI()
 @app.get("/info")
 async def health_info():
     return {"status": "UP"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
