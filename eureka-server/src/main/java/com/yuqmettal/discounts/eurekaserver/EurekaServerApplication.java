155030f3413cd17baabdf572c5d236cfbf0e8774
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-13 09:50:10 -0500

    Configure eureka server

--- eureka-server/src/main/java/com/yuqmettal/discounts/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/yuqmettal/discounts/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
