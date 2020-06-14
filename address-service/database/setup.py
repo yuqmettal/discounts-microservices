4d1efe711ef13a308b2ca6a1b8a15749b3dfb039
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-14 12:20:12 -0500

    address: update database setup

--- address-service/database/setup.py
+++ address-service/database/setup.py
@@ -1,12 +1,19 @@
+from typing import Any
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://discount_service:5b1c0d7be3c87c23e3d242a4cafd5889@localhost:5432/discounts"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+    
+    @declared_attr
+    def __tablename__(self) -> str:
+        return self.__name__.lower()
