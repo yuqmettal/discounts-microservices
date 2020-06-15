37216f746c14c94cf7c8fdf08bcb45471ee32773
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-15 16:14:04 -0500

    partners: Add eureka clients

---
+++ partners-service/client/base.py
@@ -0,0 +1,36 @@
+import requests
+from py_eureka_client import eureka_client
+
+
+class EurekaClient():
+    def __init__(self, remote_service: str):
+        self.remote_service = remote_service
+
+    def _call_post_service(self, url: str, data, /) -> 'EurekaResponse':
+        response = requests.post(url, data)
+        content = response.json()
+        return EurekaResponse(response.status, content)
+
+    def _call_get_service(self, url: str, /) -> 'EurekaResponse':
+        response = requests.get(url)
+        content = response.json()
+        return EurekaResponse(response.status, content)
+
+    def remote_service(self, uri: str, method: str, /):
+        MAP_METHODS = {
+            'POST': self._call_post_service,
+            'GET': self._call_get_service,
+        }
+
+        response = eureka_client.walk_nodes(
+            app_name=self.remote_service,
+            service=uri,
+            walker=MAP_METHODS[method]
+        )
+        return response
+
+
+class EurekaResponse:
+    def __init__(self, status: int, content: dict):
+        self.status = status
+        self.content = content
