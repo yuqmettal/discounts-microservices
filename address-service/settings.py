b3179e080b4952bb22f24786115a5ef4fc6f6d4b
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-15 12:05:47 -0500

    address: Add BASE_DIR to settings

--- address-service/settings.py
+++ address-service/settings.py
@@ -1,5 +1,7 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DATABASE_URL = os.environ.get(
     'DATABASE_URL',
     'postgresql://discount_service:5b1c0d7be3c87c23e3d242a4cafd5889@localhost:5432/dev'
