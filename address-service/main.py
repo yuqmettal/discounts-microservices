4e0587233ca4090e6285ce266b58d9965bcc05b9
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-15 12:48:49 -0500

    address: Implement eureka client

--- address-service/main.py
+++ address-service/main.py
@@ -1,9 +1,11 @@
 import uvicorn
 from fastapi import FastAPI, Depends
 from dotenv import load_dotenv
+from py_eureka_client import eureka_client
 
 load_dotenv()
 
+import settings
 from database import engine, models
 from api.v1 import api_router as v1_router
 from api.health import api_router as health_router
@@ -20,5 +22,13 @@ app.include_router(health_router)
 app.include_router(v1_router, prefix='/api/v1')
 
 
+eureka_client.init(
+    eureka_server=settings.EUREKA_SERVER,
+    app_name="address-service",
+    instance_port=int(settings.APP_PORT),
+    instance_ip=settings.APP_HOST
+)
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=settings.APP_PORT)
--- address-service/settings.py
+++ address-service/settings.py
@@ -2,7 +2,12 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+APP_HOST = os.environ.get('APP_HOST', '127.0.0.1')
+APP_PORT = os.environ.get('APP_PORT', 8081)
+
 DATABASE_URL = os.environ.get(
     'DATABASE_URL',
     'postgresql://discount_service:5b1c0d7be3c87c23e3d242a4cafd5889@localhost:5432/dev'
 )
+
+EUREKA_SERVER = os.environ.get('EUREKA_SERVER', 'http://localhost:8761/eureka')
