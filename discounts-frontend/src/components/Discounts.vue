b5bb0325da54d80c4c19dd5722b3bf51f49d03f5
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-19 13:22:09 -0500

    frontend: Add vuetify

---
+++ discounts-frontend/src/components/Discounts.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <h1>Discounts</h1>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Discounts',
+  data() {
+
+  },
+};
+</script>
