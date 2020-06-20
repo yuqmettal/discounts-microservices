6531a8eee48c985e2b7faf3617c9f03cf8b789af
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-20 10:03:46 -0500

    frontend add select to create discount form

--- discounts-frontend/src/components/Discounts.vue
+++ discounts-frontend/src/components/Discounts.vue
@@ -1,29 +1,155 @@
 <template>
   <v-container>
     <v-card class="mx-auto" outlined>
-      <h1>Discounts</h1>
-      <v-data-table
-        :headers="headers"
-        :items="discounts"
-        class="elevation-1"
-      ></v-data-table>
+      <v-data-table :headers="headers" :items="discounts" class="elevation-1">
+        <template v-slot:top>
+          <v-toolbar flat color="black">
+            <v-toolbar-title>Descuentos</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">Nuevo</v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">Descuento</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="12" md="12">
+                        <v-text-field v-model="editedItem.discount.name" label="Nombre" required />
+                      </v-col>
+                      <v-col cols="12" sm="12" md="12">
+                        <v-text-field
+                          v-model="editedItem.discount.description"
+                          label="Descripcion"
+                        />
+                      </v-col>
+                      <v-col cols="12" sm="12" md="12">
+                        <v-select
+                          v-model="editedItem.categories"
+                          :items="categoriesForSelect"
+                          label="Categorias"
+                          multiple
+                          chips
+                          hint="Selecciona las categorias que aplicaran descuento"
+                          persistent-hint
+                          deletable-chips
+                        />
+                      </v-col>
+                      <v-col cols="12" sm="12" md="12">
+                        <v-select
+                          v-model="editedItem.subcategories"
+                          :items="subcategoriesForSelect"
+                          label="Subcategorias"
+                          multiple
+                          chips
+                          hint="Selecciona las subcategorias que aplicaran descuento"
+                          persistent-hint
+                          deletable-chips
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)">
+            mdi-delete
+          </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </template>
+      </v-data-table>
     </v-card>
   </v-container>
 </template>
 
 <script>
+import discountMixin from '../mixins/discount-mixin';
+
 export default {
   name: 'Discounts',
-  async created() {
-    await this.$store.dispatch('discount/getDiscounts');
-  },
-  computed: {
-    discounts() {
-      return this.$store.getters['discount/discounts'];
-    },
-  },
+  mixins: [discountMixin],
   data: () => ({
-    headers: [{ text: 'Id', value: 'id' }],
+    headers: [
+      { text: 'Id', value: 'id' },
+      { text: 'Fecha Inicio', value: 'start_date' },
+      { text: 'Fecha Fin', value: 'end_date' },
+      { text: 'Por clientes', value: 'by_clients' },
+      { text: '', value: 'actions', sortable: false },
+    ],
+    editedIndex: -1,
+    editedItem: {
+      discount: {
+        name: '',
+        description: '',
+        start_date: '2020-06-20',
+        end_date: '2020-06-20',
+        calendarized: false,
+        priority: 0,
+        discount: 0,
+        retailer_id: 0,
+        by_categories: false,
+        by_subcategories: false,
+        by_brands: false,
+        by_products: false,
+        by_clients: false,
+        to_prime_clients: false,
+        free_shipping: false,
+        free_shipping_amount: 0,
+        according_deliver_day: false,
+        according_order_day: false,
+        order_and_deliver_same_day: false,
+      },
+      categories: [],
+      subcategories: [],
+      brands: [],
+      products: [],
+    },
+    defaultItem: {
+      discount: {
+        name: '',
+        description: '',
+        start_date: '2020-06-20',
+        end_date: '2020-06-20',
+        calendarized: false,
+        priority: 0,
+        discount: 0,
+        retailer_id: 0,
+        by_categories: false,
+        by_subcategories: false,
+        by_brands: false,
+        by_products: false,
+        by_clients: false,
+        to_prime_clients: false,
+        free_shipping: false,
+        free_shipping_amount: 0,
+        according_deliver_day: false,
+        according_order_day: false,
+        order_and_deliver_same_day: false,
+      },
+      categories: [],
+      subcategories: [],
+      brands: [],
+      products: [],
+    },
   }),
 };
 </script>
--- discounts-frontend/src/mixins/discount-mixin.js
+++ discounts-frontend/src/mixins/discount-mixin.js
@@ -12,11 +12,43 @@ export default {
     categories() {
       return this.$store.getters['category/categories'];
     },
+    categoriesForSelect() {
+      return this.categories.map((category) => ({ value: category.id, text: category.name }));
+    },
     subcategories() {
       return this.$store.getters['subcategory/subcategories'];
     },
+    subcategoriesForSelect() {
+      return this.subcategories
+        .map((subcategory) => ({ value: subcategory.id, text: subcategory.name }));
+    },
     retailers() {
       return this.$store.getters['retailer/retailers'];
+    },
+  },
+  methods: {
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.discounts[this.editedIndex], this.editedItem);
+      } else {
+        this.discounts.push(this.editedItem);
+      }
+      this.close();
+    },
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = { ...this.defaultItem };
+        this.editedIndex = -1;
+      });
+    },
+    editItem(item) {
+      this.editedIndex = this.discounts.indexOf(item);
+      this.editedItem = { ...item };
+      this.dialog = true;
+    },
+    deleteItem(item) {
+      return item;
     },
   },
 };
